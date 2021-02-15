802cc1f736dcc861b6fde71d50edf4e1e0bdafbd
leobossi <77490159+leobossi@users.noreply.github.com>
2021-02-15 16:36:18 +0000

    Update filter file.py

--- filter file.py
+++ filter file.py
@@ -1,4 +1,12 @@
-import scipy as sp
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 11 11:22:05 2021
+
+@author: leonardobossi1
+"""
+
+import scipy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
@@ -22,46 +30,63 @@ def linterpol(wavelength, wavedata, indexdata):
             break # linear extrapolation
 
     interpol = grad*(indexdata[j] - indexdata[j-1]) + indexdata[j]
+    
     return interpol
 
+
 def sellmeier(wavelength, a1, a2, a3, b1, b2, b3):
-    index2 = 1 + (a1*wavelength**2)/(wavelength**2 - b1) + (a2*wavelength**2)/(wavelength**2 - b2) +\
-             (a3*wavelength**2)/(wavelength**2 - b3)
+    n_squared = 1 + (a1 * wavelength ** 2)/(wavelength ** 2 - b1) + (a2 * wavelength **2 )/ (wavelength ** 2 - b2) +\
+             (a3 * wavelength ** 2)/(wavelength ** 2 - b3)
 
-    index = sp.sqrt(index2)
-    return index
+    n = np.sqrt(n_squared)
+    
+    return n
 
 
-wavelength, n, k= sp.loadtxt("BK7.txt", skiprows = 1, unpack = True)
-print(wavelength, n, k )
-wavelength = sp.array(wavelength) # to avoid problems with curvefit make everything into arrays
-n = sp.array(n)
-k = sp.array(k)
+wavelength, n, k= np.loadtxt("BK7.txt", skiprows = 1, unpack = True)
+wavelength = np.array(wavelength) # to avoid problems with curvefit make everything into arrays
 
-googled = list([1.03961212,0.231792344,1.01046945,6.00069867*10**3,2.00179144*10**4,1.03560653*10**8])
+n = np.array(n)
+k = np.array(k)
+
+# Converted C coefficients to nanometers squared.
+googled = list([1.03961212, 0.231792344, 1.01046945, 6.00069867e3, 2.00179144e4 , 1.03560653e8])
 
 #popt, pcov = curve_fit(sellmeier, wavelength, n, p0 = [1,0,1,0,0,100]) # initial guess taken from internet
 
-trial = sp.arange(330, 2500)
+# Trial wavelength range
+trial = np.arange(330, 2500, 0.2)
 interplovals = list(map(lambda x: linterpol(x, wavelength, n), trial))
 sellmeiervals = list(map(lambda x: sellmeier(x, *googled), trial))
 
+plt.figure()
 plt.plot(trial, interplovals)
 plt.plot(trial, sellmeiervals)
-plt.show()
-
+plt.grid()
 residuals = []
 
 for i in range(len(trial)):
     residuals.append(interplovals[i] - sellmeiervals[i])
 
-print("The averaage residual is " + str(sp.mean(residuals)))
+print("The averaage residual is " + str(np.mean(residuals)))
 
+plt.figure()
 plt.scatter(trial, residuals)
-plt.show()
+plt.title('Residual values between using linear interpolating and Sellmeier equation')
+plt.grid()
 
-plt.plot(wavelength, n)
-plt.show()
+plt.figure()
+plt.title('Scatter plot of real values from refractive indices')
+plt.plot(wavelength, n, 'x', label = 'real values for BK7 glass')
+plt.xlabel('Wavelength (nm)')
+plt.ylabel('refractive index')
+plt.grid()
+plt.legend()
 
-plt.plot(wavelength, k)
-plt.show()
+plt.figure()
+plt.title('Scatter plot of imaginary values from refractive indices')
+plt.xlabel('Wavelength (nm)')
+plt.ylabel('refractive index')
+plt.plot(wavelength, k, 'x', label = 'imaginary values for BK7 glass')
+plt.grid()
+
