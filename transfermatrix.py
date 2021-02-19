f0e5fc73ac4fa63b29ff471d6ad5da1a06b9507d
leobossi <77490159+leobossi@users.noreply.github.com>
2021-02-19 12:57:02 +0000

    Update transfermatrix.py

--- transfermatrix.py
+++ transfermatrix.py
@@ -9,8 +9,8 @@ Created on Fri Feb 19 12:28:54 2021
 import numpy as np
 
 def linterpol(wavelength, wavedata, indexdata):
-        '''
-    Args:
+    '''
+    Parameters:
         wavelength:: float
         wavedata:: numpy.array
             Discrete wavelength values used to interpolate
@@ -19,7 +19,6 @@ def linterpol(wavelength, wavedata, indexdata):
     Returns:
         Interpolated refractive index value for given wavelength.
     '''
-
     grad = 0 #initialise grad
     j = 0 #initialise j
 
@@ -52,6 +51,20 @@ def sellmeier(wavelength, a1, a2, a3, b1, b2, b3):
 
 
 def complx_n(lam, lam_data, real_data, img_data):
+    '''
+    Parameters:
+        lam:: float
+            wavelength
+        lam_data:: np.array
+            array of wavelength values obtained from online resources
+        real_data:: np.array
+            corresponding real values of refractive index
+        img_data:: np.array
+            corresponding imaginary values of refractive index
+    Returns:
+        n:: complex
+            complex refractive index at provided wavelength.
+    '''
     
     if lam > lam_data[-1] or lam < lam_data[0]:
         raise Exception('Inputted value is out of range provided in the data')
@@ -64,7 +77,24 @@ def complx_n(lam, lam_data, real_data, img_data):
 
 
 def chifunction (k0, kx, polarisation, ncomplex1, ncomplex2):
+    '''
+    Parameters:
+        k0:: float
+            Wave number in free space.
+        kx:: float
+            k in the x direction.
+        polarisation:: string
+            polarisation of wave (can be p or s polarised).
+        ncomplex1:: complex
+        ncomplex2:: complex
+            Complex refractive indices of the two materials.
+    Returns:
+        chim:: float
+            Chi_minus (to be used in the interfacial matrix in TMM.)
+        chip:: float
+            Chi_plus (to be used in the interfacial matrix in TMM.)
 
+    '''
     n1 = np.real(ncomplex1)
     n2 = np.real(ncomplex2)
 
@@ -86,11 +116,28 @@ def chifunction (k0, kx, polarisation, ncomplex1, ncomplex2):
     else:
         raise Exception("That is not a supported polarisation of light")
     
-    return chip, chim # returns chi plus and chi minuts    
+    return chip, chim  
     
 
-def TMM(wavelength, angle, polarisation, ns, ds):
 
+def TMM(wavelength, angle, polarisation, ns, ds):
+    '''
+    Parameters:
+        wavelength:: float
+            wavelength of incoming wave
+        angle:: float
+            incident angle.
+        polarisation:: string
+        ns:: list
+            list of refractive indices for all the layers in the multi-layered stack.
+        ds:: list
+            list of all separations between the layers.
+    Returns:
+        r:: float
+            Reflection coefficient.
+        t:: float
+            Transmission coefficient. 
+    '''
     if polarisation != 's' and polarisation != 'p':
         raise Exception("That is not a supported polarisation of light")
 
